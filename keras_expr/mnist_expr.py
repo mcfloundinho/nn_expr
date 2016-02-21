4c0457717e0cc0c4a9848bd2ce7de2aff6d7f1fa
Weiran He <hwr@megvii.com>
2016-02-21 20:49:22 +0800

    more reasonable mnist model

--- keras_expr/mnist_expr.py
+++ keras_expr/mnist_expr.py
@@ -6,12 +6,11 @@
 from data import mnist
 import numpy as np
 import os
-import pickle
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten,\
-    Convolution2D, MaxPooling2D
-from keras.optimizers import SGD
+    Convolution2D, MaxPooling2D, Lambda
+from keras.optimizers import Adam
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -19,7 +18,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", default="mlp", type=str,
                     help="type of model")
-parser.add_argument("--learning-rate", default=0.1, type=float)
+parser.add_argument("--learning-rate", default=0.001, type=float)
 args = parser.parse_args()
 
 
@@ -29,10 +28,10 @@ def one_hot(x, max_len=10):
     return a
 
 
-def get_dataset(gnr, is_flatten=False):
+def get_dataset(gnr):
     imgs, labels = [], []
     for img, label in gnr:
-        data = img.flatten() if is_flatten else img.reshape((1,) + img.shape)
+        data = img.reshape((1,) + img.shape)  # CHW
         imgs.append(data)
         labels.append(one_hot(label))
     imgs = np.array(imgs, 'uint8')
@@ -40,9 +39,11 @@ def get_dataset(gnr, is_flatten=False):
     return imgs, labels
 
 
-def get_model_mlp(in_dim, out_dim):
+def get_model_mlp(in_shape, out_dim):
     model = Sequential()
-    model.add(Dense(64, input_dim=in_dim, init='uniform'))
+    model.add(Lambda(lambda x: x / 255. - 0.5, input_shape=in_shape))
+    model.add(Flatten())
+    model.add(Dense(64, init='uniform'))
     model.add(Activation('tanh'))
     model.add(Dropout(0.5))
     model.add(Dense(64, init='uniform'))
@@ -57,7 +58,8 @@ def get_model_cnn(in_shape, out_dim):
     model = Sequential()
     # input: 100x100 images with 3 channels -> (3, 100, 100) tensors.
     # this applies 32 convolution filters of size 3x3 each.
-    model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=in_shape))
+    model.add(Lambda(lambda x: x / 255. - 0.5, input_shape=in_shape))
+    model.add(Convolution2D(32, 3, 3, border_mode='valid'))
     model.add(Activation('relu'))
     model.add(Convolution2D(32, 3, 3))
     model.add(Activation('relu'))
@@ -93,32 +95,35 @@ def get_model(model_type, *args, **kwargs):
 
 if __name__ == "__main__":
     # prepare data
-    is_flatten = args.model == "mlp"
-    X_train, y_train = get_dataset(mnist.trainset(), is_flatten)
-    X_test, y_test = get_dataset(mnist.testset(), is_flatten)
+    X_train, y_train = get_dataset(mnist.trainset())
+    X_test, y_test = get_dataset(mnist.testset())
     # train model
-    model = get_model(
-        args.model,
-        X_train.shape[1] if is_flatten else X_train.shape[1:],
-        y_train.shape[1],
+    model = get_model(args.model, X_train.shape[1:], y_train.shape[1])
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer=Adam(
+            lr=args.learning_rate,
+            beta_1=0.9, beta_2=0.999, epsilon=1e-08,
+        ),
     )
-    sgd = SGD(lr=args.learning_rate, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=sgd)
-    model.fit(X_train, y_train,
-              nb_epoch=20,
-              batch_size=16,
-              show_accuracy=True)
+    model.fit(
+        X_train, y_train,
+        validation_data=(X_test, y_test),
+        nb_epoch=20,
+        batch_size=128,
+        show_accuracy=True,
+    )
     # dump model
-    pickle.dump(
-        model,
-        open(os.path.join(
-            BASE_DIR,
-            "./trained_models/mnist_{}.pickle".format(args.model)
-        ), "w"),
-    )
+    open(os.path.join(
+        BASE_DIR,
+        './trained_models/mnist_{}_architecture.json'.format(args.model)
+    ), 'w').write(model.to_json())
+    model.save_weights(os.path.join(
+        BASE_DIR,
+        './trained_models/mnist_{}_weights.h5'.format(args.model)
+    ), overwrite=True)
     # eval results
-    objective_score = model.evaluate(X_test, y_test, batch_size=16)
-    classes = model.predict_classes(X_test, batch_size=32)
-    proba = model.predict_proba(X_test, batch_size=32)
+    #objective_score = model.evaluate(X_test, y_test, batch_size=16)
+    #classes = model.predict_classes(X_test, batch_size=32)
+    #proba = model.predict_proba(X_test, batch_size=32)
     from IPython import embed; embed()
