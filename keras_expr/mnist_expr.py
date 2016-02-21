9cd8c09acffbff801333dd4aeb45e4d12643931e
hewr1993 <hewr2010@gmail.com>
2016-02-21 11:57:49 +0800

    cnn support for mnist

--- keras_expr/mnist_expr.py
+++ keras_expr/mnist_expr.py
@@ -7,7 +7,18 @@ from data import mnist
 import numpy as np
 import os
 
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten,\
+    Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", default="cnn", type=str,
+                    help="type of model")
+args = parser.parse_args()
 
 
 def one_hot(x, max_len=10):
@@ -16,10 +27,11 @@ def one_hot(x, max_len=10):
     return a
 
 
-def get_dataset(gnr):
+def get_dataset(gnr, is_flatten=False):
     imgs, labels = [], []
     for img, label in gnr:
-        imgs.append(img.flatten())
+        data = img.flatten() if is_flatten else img.reshape((1,) + img.shape)
+        imgs.append(data)
         labels.append(one_hot(label))
     imgs = np.array(imgs, 'uint8')
     labels = np.array(labels, 'float32')
@@ -27,10 +39,6 @@ def get_dataset(gnr):
 
 
 def get_model_mlp(in_dim, out_dim):
-    from keras.models import Sequential
-    from keras.layers import Dense, Dropout, Activation
-    from keras.optimizers import SGD
-
     model = Sequential()
     model.add(Dense(64, input_dim=in_dim, init='uniform'))
     model.add(Activation('tanh'))
@@ -40,26 +48,74 @@ def get_model_mlp(in_dim, out_dim):
     model.add(Dropout(0.5))
     model.add(Dense(out_dim, init='uniform'))
     model.add(Activation('softmax'))
-
-    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=sgd)
     return model
+
+
+def get_model_cnn(in_shape, out_dim):
+    model = Sequential()
+    # input: 100x100 images with 3 channels -> (3, 100, 100) tensors.
+    # this applies 32 convolution filters of size 3x3 each.
+    model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=in_shape))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 3, 3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(64, 3, 3, border_mode='valid'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 3, 3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    # Note: Keras does automatic shape inference.
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(out_dim))
+    model.add(Activation('softmax'))
+
+    return model
+
+
+def get_model(model_type, *args, **kwargs):
+    if model_type == "cnn":
+        return get_model_cnn(*args, **kwargs)
+    elif model_type == "mlp":
+        return get_model_mlp(*args, **kwargs)
+    else:
+        raise NotImplementedError(model_type)
 
 if __name__ == "__main__":
     # prepare data
-    X_train, y_train = get_dataset(mnist.trainset())
-    X_test, y_test = get_dataset(mnist.testset())
+    is_flatten = args.model == "mlp"
+    X_train, y_train = get_dataset(mnist.trainset(), is_flatten)
+    X_test, y_test = get_dataset(mnist.testset(), is_flatten)
     # train model
-    model = get_model_mlp(X_train.shape[1], y_train.shape[1])
+    model = get_model(
+        args.model,
+        X_train.shape[1] if is_flatten else X_train.shape[1:],
+        y_train.shape[1],
+    )
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=sgd)
     model.fit(X_train, y_train,
               nb_epoch=20,
               batch_size=16,
               show_accuracy=True)
     # dump model
     import pickle
-    pickle.dump(model, open(
-        os.path.join(BASE_DIR, "./trained_models/mnist_mlp.pickle"), "w"))
+    pickle.dump(
+        model,
+        open(os.path.join(
+            BASE_DIR,
+            "./trained_models/mnist_{}.pickle".format(args.model)
+        ), "w"),
+    )
     # eval results
     objective_score = model.evaluate(X_test, y_test, batch_size=16)
     classes = model.predict_classes(X_test, batch_size=32)
