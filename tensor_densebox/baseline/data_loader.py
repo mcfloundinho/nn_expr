df15bac3235d6f5784c6f36f1787d373fa309d44
Wayne Ho <hewr2010@gmail.com>
2016-08-08 07:52:06 -0400

    data provider for tensor densebox on wider face done

---
+++ tensor_densebox/baseline/data_loader.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python2
+# -*- coding:utf-8 -*-
+# Created Time: Thu 21 Apr 2016 11:28:50 PM CST
+# Mail: hewr2010@gmail.com
+import os
+import sys
+import cv2
+import glob
+import time
+import numpy as np
+from data import wider_face_densebox
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+from tensorpack.dataflow.base import DataFlow
+from tensorpack.utils import get_rng
+
+
+class WiderFaceDenseBoxDataset(DataFlow):
+    def __init__(self, train_or_test='train'):
+        self.train_or_test = train_or_test
+        self.reset_state()
+
+    def reset_state(self):
+        self.rng = get_rng(self)
+
+    def size(self):
+        return wider_face_densebox.size_of_dataset(self.train_or_test)
+
+    def get_data(self):
+        for img, label in wider_face_densebox.get_dataset(
+                self.train_or_test,
+                randomize=True,
+        ):
+            data = img.astype('float32') / 255.
+            label = label.astype('float32')
+            yield [data, label]
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dataset', default='test', type=str)
+    args = parser.parse_args()
+
+    ds = WiderFaceDenseBoxDataset(args.dataset)
+    print(ds.size())
+    st_time = time.time()
+    for i, (data, label) in enumerate(ds.get_data()):
+        print "\r%d\t%.4fs" % (i, time.time() - st_time),
+        st_time = time.time()
+        sys.stdout.flush()
